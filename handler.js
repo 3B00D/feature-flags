550ac69cd0c7e5da2845d63ec1029b15f96edbe6
3B00D <ab00d_sy@hotmail.com>
2021-03-21 17:11:38 +0800

    Adding logic to load features by country or user id

--- handler.js
+++ handler.js
@@ -1,15 +1,60 @@
 'use strict';
+const { exception } = require('console');
+const fs = require('fs');
+const flagsPath = 'flags';
 
-export async function index(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const loadConfig = function (event) {
+  const feature = event.pathParameters.feature;
+  const featurePath = `${flagsPath}/${feature}.json`;
+  if (fs.existsSync(featurePath)) {
+    const config = JSON.parse(fs.readFileSync(featurePath));
+    const user_id = (event.queryStringParameters)? event.queryStringParameters.user_id : null;
+    if (user_id) {
+      if (config.users[user_id]) {
+        return config.users[user_id];
+      }
+    }
+    if (config.countries[event.headers['CloudFront-Viewer-Country']]) {
+      return config.countries[event.headers['CloudFront-Viewer-Country']];
+    } else {
+      return config.default;
+    }
+  } else {
+    throw "FEATURE_NOT_FOUND";
+  }
+}
+module.exports.index = async function (event) {
+  try {
+    const config = loadConfig(event)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({value: config})
+    };
+  } catch (error) {
+    console.log(error)
+    if (error == "FEATURE_NOT_FOUND") {
+      return {
+        statusCode: 404,
+        body: JSON.stringify(
+          {
+            message: "Feature not found."
+          },
+          null,
+          2
+        ),
+      };
+    } else {
+      console.log(error)
+      return {
+        statusCode: 503,
+        body: JSON.stringify(
+          {
+            message: "unknown error"
+          },
+          null,
+          2
+        )
+      };
+    }
+  }
 }
